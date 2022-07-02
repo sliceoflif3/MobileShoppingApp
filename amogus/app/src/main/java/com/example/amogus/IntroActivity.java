be9ed3903fe9d27850a2eb1074d711e7c96d8c9f
cattail <yunomiclover@gmail.com>
2022-07-03 01:59:19 +0700

    create introActivity

---
+++ amogus/app/src/main/java/com/example/amogus/IntroActivity.java
@@ -0,0 +1,25 @@
+package com.example.amogus;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        ConstraintLayout startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
