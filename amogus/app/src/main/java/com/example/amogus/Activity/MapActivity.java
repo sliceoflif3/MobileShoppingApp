b7202b450470019f626ce60421d3c19edee609da
mbfibat <mbfibat@gmail.com>
2022-07-05 18:43:42 +0700

    Sửa lại marker trên google map

--- amogus/app/src/main/java/com/example/amogus/Activity/MapActivity.java
+++ amogus/app/src/main/java/com/example/amogus/Activity/MapActivity.java
@@ -43,26 +43,30 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
 
-        LatLng GearVNShowRoom1 = new LatLng(10.799014337474052, 106.6474469093028);
-        map.addMarker(new MarkerOptions().position(GearVNShowRoom1).title("GEARVN"));
-        LatLng GearVNShowRoom2 = new LatLng(10.8595538925561, 106.75938814380018);
-        map.addMarker(new MarkerOptions().position(GearVNShowRoom2).title("GEARVN"));
-        LatLng GearVNShowRoom3 = new LatLng(10.753428829096821, 106.6741759739076);
-        map.addMarker(new MarkerOptions().position(GearVNShowRoom3).title("GEARVN"));
-        LatLng PhongVuShowRoom1 = new LatLng(10.77386961897219, 106.68928423979033);
-        map.addMarker(new MarkerOptions().position(PhongVuShowRoom1).title("PHONGVU"));
-        LatLng PhongVuShowRoom2 = new LatLng(10.796503689290953, 106.647063037684083);
-        map.addMarker(new MarkerOptions().position(PhongVuShowRoom2).title("PHONGVU"));
-        LatLng XGearShowRoom1 = new LatLng(10.792363992752145, 106.65586318417914);
-        map.addMarker(new MarkerOptions().position(XGearShowRoom1).title("XGEAR"));
-        LatLng TheGioiDiDongShowRoom1 = new LatLng(10.796746315286683, 106.66482807282472);
-        map.addMarker(new MarkerOptions().position(TheGioiDiDongShowRoom1).title("THEGIOIDIDONG"));
-        LatLng TheGioiDiDongShowRoom2 = new LatLng(10.787961565245752, 106.66250105773183);
-        map.addMarker(new MarkerOptions().position(TheGioiDiDongShowRoom2).title("THEGIOIDIDONG"));
-        LatLng TheGioiDiDongShowRoom3 = new LatLng(10.75983938492402, 106.66925599094226);
-        map.addMarker(new MarkerOptions().position(TheGioiDiDongShowRoom3).title("THEGIOIDIDONG"));
+        LatLng Amogus1 = new LatLng(10.799014337474052, 106.6474469093028);
+        map.addMarker(new MarkerOptions().position(Amogus1).title("Amogus1"));
+        LatLng Amogus2 = new LatLng(10.8595538925561, 106.75938814380018);
+        map.addMarker(new MarkerOptions().position(Amogus2).title("Amogus2"));
+        LatLng Amogus3 = new LatLng(10.753428829096821, 106.6741759739076);
+        map.addMarker(new MarkerOptions().position(Amogus3).title("Amogus3"));
+        LatLng Amogus4 = new LatLng(10.77386961897219, 106.68928423979033);
+        map.addMarker(new MarkerOptions().position(Amogus4).title("Amogus4"));
+        LatLng Amogus5 = new LatLng(10.796503689290953, 106.647063037684083);
+        map.addMarker(new MarkerOptions().position(Amogus5).title("Amogus5"));
+        LatLng Amogus6 = new LatLng(10.792363992752145, 106.65586318417914);
+        map.addMarker(new MarkerOptions().position(Amogus6).title("Amogus6"));
+        LatLng Amogus7 = new LatLng(10.796746315286683, 106.66482807282472);
+        map.addMarker(new MarkerOptions().position(Amogus7).title("Amogus7"));
+        LatLng Amogus8 = new LatLng(10.787961565245752, 106.66250105773183);
+        map.addMarker(new MarkerOptions().position(Amogus8).title("Amogus8"));
+        LatLng Amogus9 = new LatLng(10.75983938492402, 106.66925599094226);
+        map.addMarker(new MarkerOptions().position(Amogus9).title("Amogus9"));
+        LatLng Amogus10 = new LatLng(10.785762643551216, 106.64185037775349);
+        map.addMarker(new MarkerOptions().position(Amogus10).title("Amogus10"));
+        LatLng Amogus11 = new LatLng(10.755702063018425, 106.63463060802191);
+        map.addMarker(new MarkerOptions().position(Amogus11).title("Amogus11"));
 
-        float zoom = 10;
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(GearVNShowRoom1, zoom));
+        float zoom = 11;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Amogus5, zoom));
     }
 }
